46b6a538cc66d4daa0a5493411ff1360bc9bc4e3
kwon <qja1998@naver.com>
2024-08-28 23:25:29 +0900

    5255 타일 붙이기 풀이 완료

--- SWEA/모의_역량_테스트/5255_타일_붙이기/kwon.py
+++ SWEA/모의_역량_테스트/5255_타일_붙이기/kwon.py
@@ -1,26 +1,31 @@
 test_case = int(input())
 
-block_dict = {
-    1: 1,
-    2: 2,
-    3: 2,
-    4: 3
-}
+memo = {}
 
 def fill_tile(n, selected=[]):
     if n == 0:
-        return
+        return 1
     
+    if n in memo:
+        return memo[n]
+    
+    cnt = 0
     if n >= 3:
-        fill_tile(n-3, selected+[4])
+        cnt += fill_tile(n-3, selected+[4])
 
     if n >= 2:
-        fill_tile(n-2, selected+[2])
-        fill_tile(n-2, selected+[3])
+        cnt += fill_tile(n-2, selected+[2])
+        cnt += fill_tile(n-2, selected+[3])
     
     if n >= 1:
-        fill_tile(n-1, selected+[1])
+        cnt += fill_tile(n-1, selected+[1])
+
+    memo[n] = cnt
+
+    return cnt
+    
 
 for t in range(test_case):
     n = int(input())
 
+    print(f"#{t+1} {fill_tile(n)}")
