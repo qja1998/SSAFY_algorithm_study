a7a0a9f3f3806832d61c4bac7eb65b076fd6a6e9
kwon <qja1998@naver.com>
2024-09-10 23:22:27 +0900

    12865 평범한 배낭 풀이 완료

---
+++ BOJ/Gold/12865. 평범한 배낭/kwon.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+
+dp = [0] * (K+1)
+
+for i in range(N):
+    W, V = map(int, input().split())
+
+    for i in range(K, W-1, -1):
+        dp[i] = max(dp[i], dp[i-W]+V)
+
+print(max(dp))
