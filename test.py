fde692798859a079091c5a40d467f7b1e0dd50ee
kwon <qja1998@naver.com>
2024-09-13 00:27:32 +0900

    9539. Escape 풀이중

--- test.py
+++ test.py
@@ -2,62 +2,44 @@ import sys
 from collections import defaultdict
 from heapq import heappop, heappush
 
-INF = 1100100100100100100
+# 33_554_432
+INF = float('inf')
 
-class Asset:
-    def __init__(self, required, gain):
-        self.required = required
-        self.gain = gain
-        self.uid = global_uid[0]
-        global_uid[0] += 1
+def dfs(node, parent, values, adj):
+    main = []
 
-    def __lt__(self, other):
-        if self.required != other.required:
-            return self.required < other.required
-        return self.uid < other.uid
+    # 자식 노드에 대해 DFS 실행
+    for neighbor in adj[node]:
+        if neighbor != parent:
+            sub_assets = dfs(neighbor, node, values, adj)
 
-global_uid = [0]
+            # 자산 병합: 더 큰 자원 집합을 main으로 설정
+            if len(main) < len(sub_assets):
+                main, sub_assets = sub_assets, main
 
-class Node:
-    def __init__(self, id):
-        self.id = id
-        self.value = 0
-        self.adj = []
+            # 작은 자원 집합을 큰 집합에 병합
+            for asset in sub_assets:
+                heappush(main, asset)
 
-    def join(self, main, other):
-        for asset in other:
-            heappush(main, Asset(asset.required, asset.gain))
+    # 현재 노드의 값 처리
+    value = values[node]
+    if value > 0:
+        heappush(main, (0, value))  # 이익이 있는 경우 힙에 추가
+    else:
+        required = -value
+        gain = value
 
-    def dfs(self, parent):
-        sets = []
-        for node in self.adj:
-            if node != parent:
-                sets.append(node.dfs(self))
+        # 힙에서 자원을 사용하며 이익을 계산
+        while main and (gain <= 0 or required >= main[0][0]):
+            asset = heappop(main)
+            required = max(required, -gain + asset[0])
+            gain += asset[1]
 
-        main = []
-        for s in sets:
-            if len(main) < len(s):
-                main = s
+        # 얻은 이익이 양수일 때만 힙에 추가
+        if gain > 0:
+            heappush(main, (required, gain))
 
-        for s in sets:
-            if s != main:
-                self.join(main, s)
-
-        if self.value > 0:
-            heappush(main, Asset(0, self.value))
-        else:
-            required = -self.value
-            gain = self.value
-
-            while main and (gain <= 0 or required >= main[0].required):
-                asset = heappop(main)
-                required = max(required, -gain + asset.required)
-                gain += asset.gain
-
-            if gain > 0:
-                heappush(main, Asset(required, gain))
-
-        return main
+    return main
 
 def solve():
     input = sys.stdin.read
@@ -72,32 +54,40 @@ def solve():
         n = int(data[index])
         index += 1
 
-        nodes = [Node(i) for i in range(n + 1)]
+        values = [0] * (n + 1)  # 노드 값 저장
+        adj = defaultdict(list)  # 인접 리스트
 
         destination = int(data[index])
         index += 1
 
+        # 노드 값 입력
         for i in range(1, n + 1):
-            nodes[i].value = int(data[index])
+            values[i] = int(data[index])
             index += 1
-        nodes[0].value = INF
 
-        nodes[0].adj.append(nodes[destination])
-        nodes[destination].adj.append(nodes[0])
+        values[0] = INF  # 루트 노드 값
+
+        # 인접 리스트 구축
+        adj[0].append(destination)
+        adj[destination].append(0)
 
         for _ in range(n - 1):
             a = int(data[index])
             b = int(data[index + 1])
             index += 2
-            nodes[a].adj.append(nodes[b])
-            nodes[b].adj.append(nodes[a])
+            adj[a].append(b)
+            adj[b].append(a)
 
-        final_assets = nodes[1].dfs(None)
+        # DFS 실행 후 자산 계산
+        final_assets = dfs(1, None, values, adj)
         have = 0
-        while final_assets and final_assets[0].required <= have:
+        
+        # 힙에서 필요한 자원 사용
+        while final_assets and final_assets[0][0] <= have:
             asset = heappop(final_assets)
-            have += asset.gain
+            have += asset[1]
 
+        # 결과 확인
         if have >= INF:
             results.append("escaped")
         else:
