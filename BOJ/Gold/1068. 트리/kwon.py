0accfd2b2ccc4268b9254265a030d8c70bd56e78
kwon <qja1998@naver.com>
2024-09-22 00:43:38 +0900

    1068 트리 풀이 완료

--- BOJ/Gold/1068. 트리/kwon.py
+++ BOJ/Gold/1068. 트리/kwon.py
@@ -1,5 +1,33 @@
+from collections import defaultdict
 N = int(input())
 
-tree = list(map(int, input().split()))
+tree = defaultdict(list)
+
+start = 0
+for node, parent in enumerate(list(map(int, input().split()))):
+    if parent == -1:
+        start = node
+        continue
+    tree[parent].append(node)
 
 remove_node = int(input())
+
+ans = 0
+
+def dfs(node):
+    global ans
+    children = tree[node]
+    if node == remove_node:
+        return
+    if len(children) == 0:
+        ans += 1
+        return
+    elif len(children) == 1 and children[0] == remove_node:
+        ans += 1
+        return
+    for child in children:
+        dfs(child)
+
+dfs(start)
+
+print(ans)
