a517e663643452824973e358c9a8a6ac99591b0a
kwon <qja1998@naver.com>
2024-10-06 00:00:26 +0900

    캐슬디펜스 풀이중

--- BOJ/Gold/17135. 캐슬 디펜스/kown.py
+++ BOJ/Gold/17135. 캐슬 디펜스/kown.py
@@ -1,4 +1,6 @@
 from collections import defaultdict, deque
+from itertools import combinations
+from copy import deepcopy
 
 dxy = [[-1, 0], [0, -1], [1, 0]]
 
@@ -9,20 +11,23 @@ enermy = []
 enermy_n = 0
 for y in range(N):
     for x, value in enumerate(map(int, input().split())):
-        enermy.append([x, y])
+        enermy.append((x, y))
         enermy_n += 1
 
 
-def shot(archer):
-    q = deque([archer, 0])
+def shot(archer, enermy):
+    q = deque([[archer, 0]])
     visited = [archer]
 
     while q:
+        print(q)
         (c_x, c_y), cnt = q.popleft()
 
         if cnt >= D:
             continue
-        if [c_x, c_y] in enermy:
+        print(enermy)
+        print(c_x, c_y)
+        if (c_x, c_y) in enermy:
             return c_x, c_y
         for dx, dy in dxy:
             nx, ny = c_x + dx, c_y + dy
@@ -30,36 +35,37 @@ def shot(archer):
             if not(0 <= nx < M and 0 <= ny < N):
                 continue
 
-            q.append((nx, ny), cnt+1)
+            q.append([(nx, ny), cnt+1])
             visited.append((nx, ny))
         
     return False
 
-def step(archers, enermy_cnt=0):
-    global max_enermy
-    if enermy_n == enermy_cnt:
-        max_enermy = max(max_enermy, enermy_cnt)
-        return
-    for i, archer in enumerate(archers):
-        shoted_enermy = shot(archer)
-        if shoted_enermy:
-            enermy[shoted_enermy] = False
-            enermy_cnt += 1
+def step(archers, enermy, enermy_n):
+    kill_cnt = 0
+    while enermy_n > 0:
+        for archer in archers:
+            shoted_pos = shot(archer, enermy)
+            if not shoted_pos:
+                continue
+            enermy.remove(shoted_pos)
+            enermy_n -= 1
+            kill_cnt += 1
+        
+        for i, (x, y) in enumerate(enermy):
+            if y + 1 >= N:
+                enermy.remove((x, y))
+                enermy_n -= 1
+                continue
+            enermy[i] = (x, y+1)
 
-        for e in enermy:
-            if enermy[e]:
-                enermy[e] = False
-                e = list(e)
-                e[1] += 1
-                if e[1] == N:
-                    enermy_cnt += 1s
-                    continue
-                enermy[tuple(e)] = True
-
-        step(archers[i:], enermy_cnt)
-
-        if shoted_enermy:
-            enermy[shoted_enermy] = True
-            enermy_cnt -= 1
-
+    return kill_cnt
+            
 max_enermy = 0
+
+for archers in combinations(range(M), 3):
+    archers = list(zip(archers, [N]*M))
+    print(archers)
+
+    max_enermy = max(max_enermy, step(deepcopy(archers), deepcopy(enermy), enermy_n))
+
+print(max_enermy)
