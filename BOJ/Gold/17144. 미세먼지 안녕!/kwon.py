cd7bb9c04b8239aa7120e5d268e2ffeef456b4e3
kwon <qja1998@naver.com>
2024-11-06 00:44:28 +0900

    17144 미세먼지 안녕! 풀이 완료

--- BOJ/Gold/17144. 미세먼지 안녕!/kwon.py
+++ BOJ/Gold/17144. 미세먼지 안녕!/kwon.py
@@ -11,7 +11,7 @@ def show(dust, purifier):
         tmp[y][x] = -1
     return tmp
 
-def dust_diffuse(dust_map, dust, R, C):
+def dust_diffuse(dust_map, R, C):
     """
     1. 미세먼지가 확산된다. 확산은 미세먼지가 있는 모든 칸에서 동시에 일어난다.
     (r, c)에 있는 미세먼지는 인접한 네 방향으로 확산된다.
@@ -21,25 +21,28 @@ def dust_diffuse(dust_map, dust, R, C):
     """
 
     diffuse = defaultdict(int)
-    for x, y in dust:
-        diffuse_dust = dust_map[y][x] // 5
-
-        for dx, dy in dxy:
-            nx, ny = x + dx, y + dy
-            # 범위 벗어남
-            if not (0 <= nx < C and 0 <= ny < R):
+    for y in range(R):
+        for x in range(C):
+            if dust_map[y][x] <= 0:
                 continue
+            diffuse_dust = dust_map[y][x] // 5
 
-            # 비어있지 않음
-            if dust_map[ny][nx] != 0:
-                continue
+            for dx, dy in dxy:
+                nx, ny = x + dx, y + dy
 
-            dust_map[y][x] -= diffuse_dust
-            diffuse[(x, y)] += diffuse_dust
+                # 범위 벗어남
+                if not (0 <= nx < C and 0 <= ny < R):
+                    continue
+
+                # 비어있지 않음
+                if dust_map[ny][nx] == -1:
+                    continue
+
+                dust_map[y][x] -= diffuse_dust
+                diffuse[(nx, ny)] += diffuse_dust
 
     for x, y in diffuse:
         dust_map[y][x] += diffuse[(x, y)]
-        dust.append((x, y))
 
 
 def do_purifier(dust_map, purifier_up, purifier_down, R, C):
@@ -60,7 +63,7 @@ def do_purifier(dust_map, purifier_up, purifier_down, R, C):
     up_line += dust_map[up_y][up_x + 1:]
     len_list.append(len(up_line))
 
-    up_line += [dust_map[i][C-1] for i in range(up_y+1, 0, -1)]
+    up_line += [dust_map[i][C-1] for i in range(up_y-1, 0, -1)]
     len_list.append(len(up_line))
 
     up_line += dust_map[0][::-1]
@@ -75,6 +78,8 @@ def do_purifier(dust_map, purifier_up, purifier_down, R, C):
     # 반시계 회전
     up_line = [0] + up_line[:-1]
 
+
+
     # map에 적용
     line_list = [up_line[:len_list[0]],
                  up_line[len_list[0]:len_list[1]],
@@ -82,25 +87,70 @@ def do_purifier(dust_map, purifier_up, purifier_down, R, C):
                  up_line[len_list[2]:len_list[3]],
                  up_line[len_list[3]:]]
     
+    # print(line_list)
 
     dust_map[up_y][up_x + 1:] = line_list[0]
 
-    print(line_list)
-
     for i in range(len_list[1] - len_list[0]):
-        dust_map[i][C-1] = len_list[1]-1-i
+        dust_map[i+1][C-1] = line_list[1][len_list[1] - len_list[0] - i - 1]
 
     dust_map[0][::-1] = line_list[2]
 
     for i in range(len_list[3] - len_list[2]):
-        dust_map[i][0] = len_list[3]-1-i
+        dust_map[i+1][0] = line_list[3][i]
 
     dust_map[up_y][:up_x] = line_list[4]
 
 
+    # 아래쪽 회전
+    down_line = []
+    down_x, down_y = purifier_down
+    
+    # 공기청정기 기준 시계로 (모서리는 가로에 포함)
+    len_list = []
+    down_line += dust_map[down_y][down_x + 1:]
+    len_list.append(len(down_line))
+
+    down_line += [dust_map[i][C-1] for i in range(down_y+1, R-1)]
+    len_list.append(len(down_line))
+
+    down_line += dust_map[R-1][::-1]
+    len_list.append(len(down_line))
+
+    down_line += [dust_map[i][0] for i in range(R-2, down_y, -1)]
+    len_list.append(len(down_line))
+
+    down_line += dust_map[down_y][0:down_x]
+    len_list.append(len(down_line))
+
+    # 시계 회전
+    down_line = [0] + down_line[:-1]
+
+    # map에 적용
+    line_list = [down_line[:len_list[0]],
+                 down_line[len_list[0]:len_list[1]],
+                 down_line[len_list[1]:len_list[2]],
+                 down_line[len_list[2]:len_list[3]],
+                 down_line[len_list[3]:]]
+    
+
+    # print(line_list)
+
+    dust_map[down_y][down_x + 1:] = line_list[0]
+
+    for i in range(len_list[1] - len_list[0]):
+        dust_map[down_y+i+1][C-1] = line_list[1][i]
+
+    dust_map[R-1][::-1] = line_list[2]
+
+    for i in range(len_list[3] - len_list[2]):
+        dust_map[down_y+i+1][0] = line_list[3][len_list[3] - len_list[2] - i - 1]
+
+    dust_map[down_y][:down_x] = line_list[4]
+
+
 R, C, T = map(int, input().split())
 
-dust = []
 purifier = []
 
 dust_map = []
@@ -109,9 +159,7 @@ for y in range(R):
     row = []
     for x, val in enumerate(map(int, input().split())):
         row.append(val)
-        if val > 0:
-            dust.append((x, y))
-        elif val == -1:
+        if val == -1:
             purifier.append((x, y))
     dust_map.append(row)
 
@@ -119,11 +167,15 @@ purifier_up = purifier[0]
 purifier_down = purifier[1]
 
 for _ in range(T):
-    dust_diffuse(dust_map, dust, R, C)
+    dust_diffuse(dust_map, R, C)
     do_purifier(dust_map, purifier_up, purifier_down, R, C)
 
 result = 0
-for x, y in dust:
-    result += dust_map[y][x]
+
+for y in range(R):
+    for x in range(C):
+        if dust_map[y][x] <= 0:
+            continue
+        result += dust_map[y][x]
 
 print(result)
